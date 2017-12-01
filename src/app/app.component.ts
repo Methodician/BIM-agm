bc0d423d932d8477298faf5906d9d9c36f382b30
Methodician <jacobgjohnston@gmail.com>
2017-11-30 18:20:54 -0800

    Initialized project and core libraries and tested core functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  items: Observable<any[]>;
+
+  constructor(db: AngularFirestore) {
+    this.items = db.collection('items').valueChanges();
+  }
+
+  ngAfterViewInit() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => this.showPosition(position));
+    }
+    else {
+      alert('Geolocation is not supported by this browser.');
+    }
+  }
+
+  showPosition(position) {
+    this.lat = position.coords.latitude;
+    this.lng = position.coords.longitude;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
+//  Module Imports:
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AgmCoreModule } from '@agm/core';
 
-
+//  Comopnent Imports
 import { AppComponent } from './app.component';
+
+//  Misc. Imports
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -10,6 +18,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase, 'bim-earth'),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyChU-B6alfi0jRmSeYMqbenOVpbDo6DYNM'
+    }),
     BrowserModule
   ],
   providers: [],
