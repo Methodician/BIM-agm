e9876623193e72617c0f702ba9843bbcdcbe1056
Methodician <jacobgjohnston@gmail.com>
2017-12-01 16:33:32 -0800

    Some basic test stuff is working and markers are being saved in simplest way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFirestore } from 'angularfire2/firestore';
+import { LatLng } from '@agm/core/services/google-maps-types';
+import * as fb from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,34 @@ import { AngularFirestore } from 'angularfire2/firestore';
 export class AppComponent {
   lat: number = 51.678418;
   lng: number = 7.809007;
-  items: Observable<any[]>;
+  markers: Observable<any>;
 
-  constructor(db: AngularFirestore) {
-    this.items = db.collection('items').valueChanges();
+  polyPath = [
+    {
+      lat: 45.505107,
+      lng: -122.746799
+    },
+    {
+      lat: 45.504910,
+      lng: -122.746016
+    },
+    {
+      lat: 45.504662,
+      lng: -122.746378
+    },
+    {
+      lat: 45.504466,
+      lng: -122.747389
+    }
+  ]
+
+  constructor(private db: AngularFirestore) {
+
+  }
+
+  ngOnInit() {
+    this.markers = this.db.collection('markers').valueChanges();
+
   }
 
   ngAfterViewInit() {
@@ -23,10 +49,20 @@ export class AppComponent {
     else {
       alert('Geolocation is not supported by this browser.');
     }
+    // this.db.collection('markers').valueChanges().subscribe((markers: any) => {
+    //   this.markers = markers;
+    // });
+  }
+
+  mapClick($e) {
+    console.log($e);
+    let geopoint = new fb.firestore.GeoPoint($e.coords.lat, $e.coords.lng);
+    this.db.collection('markers').add({ geopoint: geopoint });
   }
 
   showPosition(position) {
     this.lat = position.coords.latitude;
     this.lng = position.coords.longitude;
   }
+
 }
